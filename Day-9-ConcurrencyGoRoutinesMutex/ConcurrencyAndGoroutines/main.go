b10323d1881d0bae8e256d6abb55ccfa3b66be8b
Debadrita Ghosh <debadrita.ghosh2010@gmail.com>
2022-02-16 14:07:09 +0530

    Day-9-concurrency&waitGroup

---
+++ Day-9-ConcurrencyGoRoutinesMutex/ConcurrencyAndGoroutines/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup //pointer
+
+func main() {
+	// go greeter("Hello")
+	// greeter("world")
+
+	websiteList := []string{
+		"https://go.dev",
+		"https://google.com",
+		"https://fb.com",
+		"https://github.com",
+		"https://instagram.com",
+	}
+
+	for _, web := range websiteList {
+		go getStatusCode(web)
+		wg.Add(1)
+	}
+
+	wg.Wait()
+
+}
+
+func getStatusCode(endpoint string) {
+	defer wg.Done()
+	res, err := http.Get(endpoint)
+
+	if err != nil {
+		fmt.Println("Error in endpoint")
+	} else {
+		fmt.Printf("%d status code for %s\n", res.StatusCode, endpoint)
+	}
+}
+
+func greeter(s string) {
+	for i := 0; i < 6; i++ {
+		time.Sleep(3 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
